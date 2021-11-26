2e32de6c58b4b5248362f45f50c4efe5cc8075ba
yamap55 <tetuya120@hotmail.com>
2021-11-26 22:03:53 +0900

    [add] slack api util

---
+++ get_all_message_from_slack/util/slack_api.py
@@ -0,0 +1,163 @@
+"""Slack APIを操作する関数群"""
+from time import sleep
+from typing import Any, Callable, Dict, List, Optional
+
+from get_all_message_from_slack.settings import client
+from slack_sdk.web.slack_response import SlackResponse
+
+
+def get_channel_id(name: str) -> str:
+    """
+    指定されたチャンネルのチャンネルIDを取得
+
+    Parameters
+    ----------
+    name : str
+        チャンネル名
+
+    Returns
+    -------
+    str
+        チャンネルID
+
+    Raises
+    -------
+    ValueError
+        存在しないチャンネル名の場合
+    """
+    # https://api.slack.com/methods/conversations.list
+    try:
+        option = {}
+        next_cursor = "DUMMY"  # whileを1度は回すためダミー値を設定
+        while next_cursor:
+            response = client.conversations_list(**option).data
+            target_channnels = [
+                channel["id"] for channel in response["channels"] if channel["name"] == name
+            ]
+            if target_channnels:
+                return target_channnels[0]
+            # チャンネルが多い場合は1度で全てを取得できない
+            # 尚、メッセージ取得系と異なり「has_more」属性は持っていない
+            next_cursor = response["response_metadata"]["next_cursor"]
+            option["cursor"] = next_cursor
+            sleep(1)  # need to wait 1 sec before next call due to rate limits
+        raise ValueError("not exists channel name.")
+    except StopIteration:
+        raise ValueError("not exists channel name.")
+
+
+def get_user_name(user_id: str) -> str:
+    """
+    指定されたユーザIDのユーザ名を取得
+
+    Parameters
+    ----------
+    user_id : str
+        ユーザID
+
+    Returns
+    -------
+    str
+        ユーザ名
+
+    Raises
+    -------
+    SlackApiError
+        存在しないユーザIDの場合
+    """
+    # https://api.slack.com/methods/users.info
+    return client.users_info(user=user_id)["user"]["real_name"]
+
+
+def post_message(
+    channel_id: str, text: str, thread_ts: Optional[str] = None, mention_users: List[str] = []
+) -> Dict[str, Any]:
+    """
+    指定されたチャンネルにメッセージをポスト
+
+    Parameters
+    ----------
+    channel_id : str
+        チャンネルID
+    text : str
+        ポストする内容
+    thread_ts : Optional[str], optional
+        リプライとしたい場合に指定するタイムスタンプ, by default None
+    mention_users : List[str], optional
+        メンションを指定するユーザID
+        テキストの先頭に空白区切りで付与します
+        2人以上が指定されている場合はメンション後に改行を追加します, by default []
+
+    Returns
+    -------
+    Dict[str, Any]
+        ポストしたメッセージのデータ
+    """
+    # https://api.slack.com/methods/chat.postMessage
+    mentions = [f"<@{u}>" for u in mention_users]
+    mentions_postfix = "\n" if len(mentions) > 1 else ""
+    send_message = " ".join(mentions) + mentions_postfix + text
+
+    res = client.chat_postMessage(channel=channel_id, text=send_message, thread_ts=thread_ts)
+    return res.data
+
+
+def __get_all_message_by_iterating(
+    func: Callable[..., SlackResponse], option: Dict[str, Any]
+) -> List[Dict[str, Any]]:
+    """繰り返し処理ですべてのメッセージを取得"""
+    response = func(**option).data
+    messages_all = response["messages"]
+    while response["has_more"]:
+        sleep(1)  # need to wait 1 sec before next call due to rate limits
+        response = func(**option, cursor=response["response_metadata"]["next_cursor"]).data
+        messages = response["messages"]
+        messages_all = messages_all + messages
+    return messages_all
+
+
+def get_channel_message(channel_id: str, oldest: float, latest: float) -> List[Dict[str, Any]]:
+    """
+    指定されたチャンネルのメッセージを取得
+
+    Parameters
+    ----------
+    channel_id : str
+        チャンネルID
+    oldest : float
+        取得を行う最初の時間
+    latest : float
+        取得を行う最後の時間
+
+    Returns
+    -------
+    List[Dict[str, Any]]
+        指定されたチャンネルのメッセージ
+    """
+    # https://api.slack.com/methods/conversations.history
+    option = {"channel": channel_id, "oldest": oldest, "latest": latest}
+    return __get_all_message_by_iterating(client.conversations_history, option)
+
+
+def get_replies(channel_id: str, message: Dict[str, Any]) -> List[Dict[str, Any]]:
+    """
+    指定されたメッセージのリプライを取得
+
+    Parameters
+    ----------
+    channel_id : str
+        チャンネルID
+    message : Dict[str, Any]
+        リプライを取得する対象のメッセージ
+
+    Returns
+    -------
+    List[Dict[str, Any]]
+        リプライメッセージ
+        リプライがついていない場合は空のリスト
+    """
+    # https://api.slack.com/methods/conversations.replies
+    if "thread_ts" not in message:
+        return []
+    option = {"channel": channel_id, "ts": message["thread_ts"]}
+    return __get_all_message_by_iterating(client.conversations_replies, option)
---
+++ tests/get_all_message_from_slack/util/test_slack_api.py
@@ -0,0 +1,320 @@
+from typing import Any, Dict
+from unittest import mock
+
+import pytest
+from get_all_message_from_slack.util.slack_api import (
+    get_channel_id,
+    get_channel_message,
+    get_replies,
+    get_user_name,
+    post_message,
+)
+from slack_sdk.errors import SlackApiError
+
+
+class ReturnValue:
+    data = {}
+
+
+def create_return_object(data: Dict[str, Any]):
+    return_value = ReturnValue()
+    return_value.data = data
+    return return_value
+
+
+class TestGetUserName:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        with mock.patch(
+            "get_all_message_from_slack.util.slack_api.client.users_info",
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_nomal_case(self):
+        self.mock_method.return_value = {"user": {"real_name": "REAL_NAME"}}
+
+        actual = get_user_name("USER_ID")
+        expected = "REAL_NAME"
+
+        assert actual == expected
+        self.mock_method.assert_called_once_with(user="USER_ID")
+
+    def test_not_exists_user_id(self):
+        # APIにアクセスしたくないため、モックで例外を投げている
+        # ユニットテストとしては意味がないが、仕様記載の意味で記載しておく
+        self.mock_method.side_effect = SlackApiError("a", "b")
+
+        with pytest.raises(SlackApiError):
+            get_user_name("NOT_EXISTS_USER_ID")
+
+
+class TestGetChannelId:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+
+        self.return_value: Dict[str, Any] = {"response_metadata": {"next_cursor": ""}}
+
+        with mock.patch(
+            "get_all_message_from_slack.util.slack_api.client.conversations_list",
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_nomal_case(self):
+        self.mock_method.return_value = create_return_object(
+            {
+                "channels": [
+                    {"name": "CHANNEL_NAME1", "id": "CHANNEL_ID1"},
+                    {"name": "CHANNEL_NAME2", "id": "CHANNEL_ID2"},
+                ]
+            }
+        )
+        actual = get_channel_id("CHANNEL_NAME1")
+        expected = "CHANNEL_ID1"
+
+        assert actual == expected
+        self.mock_method.assert_called_once_with()
+
+    def test_not_exists_channel_name(self):
+        self.mock_method.return_value = create_return_object(
+            {
+                "channels": [],
+                "response_metadata": {"next_cursor": ""},
+            }
+        )
+        with pytest.raises(ValueError):
+            get_channel_id("CHANNEL_NAME")
+
+    def test_next_cursor_true(self):
+        self.mock_method.side_effect = [
+            create_return_object(
+                {
+                    "channels": [],
+                    "response_metadata": {"next_cursor": "NEXT_CURSOR"},
+                }
+            ),
+            create_return_object(
+                {
+                    "channels": [{"name": "CHANNEL_NAME1", "id": "CHANNEL_ID1"}],
+                    "response_metadata": {"next_cursor": ""},
+                }
+            ),
+        ]
+        actual = get_channel_id("CHANNEL_NAME1")
+        expected = "CHANNEL_ID1"
+
+        assert actual == expected
+        self.mock_method.assert_has_calls(
+            [
+                mock.call(),
+                mock.call(cursor="NEXT_CURSOR"),
+            ]
+        )
+
+
+class TestPostMessage:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        class ReturnValue:
+            data = {"ok": True, "ts": "1234567890.000002", "message": {}}
+
+        with mock.patch(
+            "get_all_message_from_slack.util.slack_api.client.chat_postMessage",
+            return_value=ReturnValue(),
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_all_args(self):
+        actual = post_message(
+            "CHANNEL_ID",
+            "POST_MESSAGE",
+            thread_ts="1234567890.000001",
+            mention_users=["USER_ID_1", "USER_ID_2"],
+        )
+        expected = {"ok": True, "ts": "1234567890.000002", "message": {}}
+
+        assert actual == expected
+        self.mock_method.assert_called_once_with(
+            channel="CHANNEL_ID",
+            text="<@USER_ID_1> <@USER_ID_2>\nPOST_MESSAGE",
+            thread_ts="1234567890.000001",
+        )
+
+    def test_required_args_only(self):
+        actual = post_message("CHANNEL_ID", "POST_MESSAGE")
+        expected = {"ok": True, "ts": "1234567890.000002", "message": {}}
+
+        assert actual == expected
+        self.mock_method.assert_called_once_with(
+            channel="CHANNEL_ID", text="POST_MESSAGE", thread_ts=None
+        )
+
+
+class TestGetChannelMessage:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        with mock.patch(
+            "get_all_message_from_slack.util.slack_api.client.conversations_history",
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_not_has_more(self):
+        messages = [
+            {
+                "ts": "1234567890.000001",
+                "text": "TEXT_MESSAGE_1",
+                "user": "USER_1",
+            }
+        ]
+        self.mock_method.return_value = create_return_object(
+            {
+                "has_more": False,
+                "messages": messages,
+            }
+        )
+        actual = get_channel_message("CHANNEL_ID", float(1.23), float(4.56))
+        expected = messages
+
+        assert actual == expected
+        self.mock_method.assert_called_once_with(
+            channel="CHANNEL_ID", oldest=float(1.23), latest=float(4.56)
+        )
+
+    def test_has_more(self):
+        messages_1 = [
+            {
+                "ts": "1234567890.000001",
+                "text": "TEXT_MESSAGE_1",
+                "user": "USER_1",
+            }
+        ]
+        messages_2 = [
+            {
+                "ts": "1234567890.000002",
+                "text": "TEXT_MESSAGE_2",
+                "user": "USER_2",
+            }
+        ]
+        self.mock_method.side_effect = [
+            create_return_object(
+                {
+                    "has_more": True,
+                    "messages": messages_1,
+                    "response_metadata": {"next_cursor": "abcdefg"},
+                }
+            ),
+            create_return_object(
+                {
+                    "has_more": False,
+                    "messages": messages_2,
+                }
+            ),
+        ]
+
+        actual = get_channel_message("CHANNEL_ID", float(1.23), float(4.56))
+        expected = messages_1 + messages_2
+
+        assert actual == expected
+        self.mock_method.assert_has_calls(
+            [
+                mock.call(channel="CHANNEL_ID", oldest=float(1.23), latest=float(4.56)),
+                mock.call(
+                    channel="CHANNEL_ID", oldest=float(1.23), latest=float(4.56), cursor="abcdefg"
+                ),
+            ]
+        )
+
+
+class TestGetReplies:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        with mock.patch(
+            "get_all_message_from_slack.util.slack_api.client.conversations_replies",
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_not_exists_thread_ts_in_message(self):
+        actual = get_replies("CHANNEL_ID", {})
+        expected = []
+
+        assert actual == expected
+        self.mock_method.assert_not_called()
+
+    def test_exists_thread_ts_in_message_and_not_has_more(self):
+        arg_message = {
+            "thread_ts": "1234567890.000001",
+            "ts": "1234567890.000001",
+            "text": "TEXT_MESSAGE_1",
+            "user": "USER_1",
+        }
+        messages = [
+            {
+                "ts": "1234567890.000010",
+                "text": "TEXT_MESSAGE_10",
+                "user": "USER_10",
+            }
+        ]
+        self.mock_method.return_value = create_return_object(
+            {
+                "has_more": False,
+                "messages": messages,
+            }
+        )
+        actual = get_replies("CHANNEL_ID", arg_message)
+        expected = messages
+
+        assert actual == expected
+        self.mock_method.assert_called_once_with(channel="CHANNEL_ID", ts="1234567890.000001")
+
+    def test_exists_thread_ts_in_message_and_has_more(self):
+        arg_message = {
+            "thread_ts": "1234567890.000001",
+            "ts": "1234567890.000001",
+            "text": "TEXT_MESSAGE_1",
+            "user": "USER_1",
+        }
+
+        messages_1 = [
+            {
+                "ts": "1234567890.000010",
+                "text": "TEXT_MESSAGE_10",
+                "user": "USER_1",
+            }
+        ]
+        messages_2 = [
+            {
+                "ts": "1234567890.000020",
+                "text": "TEXT_MESSAGE_20",
+                "user": "USER_2",
+            }
+        ]
+        self.mock_method.side_effect = [
+            create_return_object(
+                {
+                    "has_more": True,
+                    "messages": messages_1,
+                    "response_metadata": {"next_cursor": "abcdefg"},
+                }
+            ),
+            create_return_object(
+                {
+                    "has_more": False,
+                    "messages": messages_2,
+                }
+            ),
+        ]
+
+        actual = get_replies("CHANNEL_ID", arg_message)
+        expected = messages_1 + messages_2
+
+        assert actual == expected
+        self.mock_method.assert_has_calls(
+            [
+                mock.call(channel="CHANNEL_ID", ts="1234567890.000001"),
+                mock.call(channel="CHANNEL_ID", ts="1234567890.000001", cursor="abcdefg"),
+            ]
+        )
