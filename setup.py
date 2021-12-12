648f69e307a585d4c5c7701554627b821c3d1819
yamap55 <1785817+yamap55@users.noreply.github.com>
2021-12-12 14:56:09 +0900

    [add] build setting (#17)

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""モジュールをインストールし、コマンドとして使用できるようにする setup.py."""
+from setuptools import find_packages, setup
+
+setup(
+    name="get_all_message_from_slack",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=["slack-sdk"],
+    # コマンドが実行されたときのエントリーポイント.
+    entry_points={
+        "console_scripts": [
+            "get_all_message_from_slack=get_all_message_from_slack.main:entry_point"
+        ]
+    },
+)
