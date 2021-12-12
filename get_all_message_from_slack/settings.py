a9d390ee203eb1540953d02eace4cf410306eb79
yamap55 <1785817+yamap55@users.noreply.github.com>
2021-12-12 15:19:37 +0900

    [remove] locale settings

--- get_all_message_from_slack/settings.py
+++ get_all_message_from_slack/settings.py
@@ -1,8 +1,6 @@
 """application settings"""
-import locale
 import os
 
 from slack_sdk.web.client import WebClient
 
-locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
 client = WebClient(token=os.environ["SLACK_TOKEN"])
