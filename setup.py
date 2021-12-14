f2a07851aaefedcd5f949fce7aa629f8f12eab06
yamap55 <1785817+yamap55@users.noreply.github.com>
2021-12-14 09:57:32 +0900

    [fixed] python version 3.9 -> 3.8 (#31)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="get_all_message_from_slack",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     install_requires=["slack-sdk"],
     # コマンドが実行されたときのエントリーポイント.
     entry_points={
